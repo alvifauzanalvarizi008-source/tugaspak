76bf0e67f29df4d6452d49c29387ae267f725df9
alvifauzanalvarizi008-source <alvi.fauzanalvarizi008@gmail.com>
2025-12-18 09:30:08 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,103 @@
+// Data dummy untuk bahan ajar
+const dataBahanAjar = [
+    {
+        kode: "BA001",
+        nama: "Buku Pemogramman PHP Dasar",
+        stok: 150,
+        status: "Tersedia"
+    },
+    {
+        kode: "BA002",
+        nama: "Buku Pemogramman HTML DAN CSS",
+        stok: 75,
+        status: "Tersedia"
+    },
+    {
+        kode: "BA003",
+        nama: "Buku Pemogramman Javascript",
+        stok: 50,
+        status: "Terbatas"
+    },
+    {
+        kode: "BA004",
+        nama: "Buku Sakti PHP",
+        stok: 0,
+        status: "Habis"
+    },
+    {
+        kode: "BA005",
+        nama: "Buku Pemogramman PHP",
+        stok: 200,
+        status: "Tersedia"
+    }
+];
+
+// Data dummy untuk tracking pengiriman
+const dataPengiriman = [
+    {
+        noDO: "2023001",
+        namaMahasiswa: "Ahmad Fauzi",
+        status: "Dikirim",
+        progress: 75,
+        ekspedisi: "Pemogramman Dasar",
+        tanggalKirim: "2023-10-15",
+        jenisPaket: "Pemogramman Dasar",
+        gurupembimbing: "pak mulyono",
+        totalPembayaran: 125000
+    },
+    {
+        noDO: "2023002",
+        namaMahasiswa: "Siti Rahayu",
+        status: "Diproses",
+        progress: 30,
+        ekspedisi: "Pemogramman PHP",
+        tanggalKirim: "2023-10-20",
+        jenisPaket: "Pemogramman PHP",
+        gurupembimbing: "pak amba",
+        totalPembayaran: 185000
+    },
+    {
+        noDO: "2023003",
+        namaMahasiswa: "Budi Santoso",
+        status: "Selesai",
+        progress: 100,
+        ekspedisi: "Pemogramman Javascript",
+        tanggalKirim: "2023-10-05",
+        jenisPaket: "Pemogramman Javascript",
+        gurupembimbing: "pak rusdi",
+        totalPembayaran: 95000
+    }
+];
+
+const dataLaporan = [
+    {
+        noInduk: "2023001",
+        namaSiswa: "Fauzi",
+    
+        Alasan: "sakit",
+        mapel: "Pemogramman Dasar",
+        
+        gurupembimbing: "pak mulyono",
+        
+    },
+    {
+        noInduk: "2023002",
+        namaSiswa: "Rahayu",
+        
+        Alasan: "izin",
+        mapel: "Pemogramman PHP",
+        
+        gurupembimbing: "pak amba",
+        
+    },
+    {
+       noInduk: "2023003",
+        namaSiswa: "Faiz",
+        
+        Alasan: "izin",
+        mapel: "Pemogramman PHP",
+        
+        gurupembimbing: "pak amba",
+    }
+];
+
